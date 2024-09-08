c0980435b17fc52dac5f902f0d3c16dec5816771
Wajih <wajihqazi94@gmail.com>
2024-09-08 00:52:20 -0400

    feature/Create container for buttons and add some styling, implmented page navigation

---
+++ src/components/elementContainer/elementContainer.tsx
@@ -0,0 +1,12 @@
+import React,  { ReactNode } from "react";
+import "./elementContainer.css";
+
+const ElementContainer: React.FC = () => {
+    return (
+        <div data-testid="container" className="element_container">
+
+        </div>
+    );
+};
+
+export default ElementContainer;
